1c30f5975547de27b38b4e5e5b3974ecd8a5a79d
unknown <alex_nerob@mail.ru>
2026-05-13 10:28:02 +0500

    создание цветов

---
+++ uikit/src/main/java/com/example/uikit/presentation/Colors.kt
@@ -0,0 +1,11 @@
+package com.example.uikit.presentation
+
+import androidx.compose.ui.graphics.Color
+
+//13.05.2026
+//11
+//определение цветов
+val Primary: Color = Color(0xFF004AC6)
+val Secondary: Color = Color(0xFF505F76)
+val Tertiary: Color = Color(0xFF943700)
+val Error: Color = Color(0xFFBA1A1A)
